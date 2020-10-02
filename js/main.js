f7cbc69657396c03ea3e91b883d0db8219080e9b
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-10-02 03:51:44 +0000

    📦 NEW: Added Profile Section 🤧

---
+++ js/main.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function() {
+
+
+
+
+
+    let $logo = $('#logo');
+
+    if(location.href.indexOf('#') != -1){
+        $logo.show();
+    }
+
+    // Show Logo
+    $('.menu .tabs a').click(function() {
+        $logo.fadeIn('slow')
+    })
+    // Hide Logo
+    $('.tab-profile').click(function() {
+        $logo.fadeOut('slow')
+    })
+
+    let $content = $('#content');
+
+    $content.easytabs({
+        animate : true,
+        updateHash : false,
+        transitionIn : 'slideDown',
+        transitionOut : 'slideUp',
+        animationSpeed : 600,
+        tabs : '.tmenu',
+        tabActiveClass : 'active'
+    });
+
+
+    // Hover Effect
+    $content.find('.tabs li a').hover(
+        function(){
+            $(this).stop().animate({marginTop: '-7px'}, 200);
+        }, function(){
+            $(this).stop().animate({marginTop: '0px'}, 300)
+        }
+    );
+
+})
